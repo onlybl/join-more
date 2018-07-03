55b8f30eca890ca4ac38bdda455cc714072725e3
hyz <609927332@qq.com>
2018-07-03 14:29:58 +0800

    Initial commit

---
+++ src/main/java/com/sicaudragon/joinmore/JoinMoreApplication.java
@@ -0,0 +1,12 @@
+package com.sicaudragon.joinmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinMoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sicaudragon/joinmore/JoinMoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sicaudragon.joinmore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinMoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
